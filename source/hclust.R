52914c21d971dffb507c8a6e3fc6ea6139ce65b6
kur0cky <blacky.kur0cky@gmail.com>
2018-05-04 14:41:57 +0900

    commit

--- source/hclust.R
+++ source/hclust.R
@@ -1,6 +1,8 @@
 library(tidyverse)
 library(dtw)
 library(class)
+library(TSclust)
+library(ggdendro)
 
 betti <- read_csv("data/mazda/processed/betti_seq.csv")
 
@@ -12,12 +14,6 @@ betti1 <- betti %>%
   nest() %>% 
   arrange(name, type,number) %>% 
   mutate(ID = 1:n())
-
-data <- betti1  %>% 
-  unnest() %>% 
-  select(ID, radius, Betti) %>% 
-  spread(ID, Betti) %>% 
-  select(-radius)
 
 
 data <- betti1 %>% 
@@ -33,32 +29,19 @@ data <- betti1 %>%
                       TSclust::diss("DTWARP")),
          clust = map(dist, 
                      ~ .x %>% 
-                       hclust()))
+                       hclust(method = "complete")))
 
 for(i in 1:15){
   data$clust[[i]]$labels <- rep(c("after", "before"), each=10)
 }
 
-plot(data$clust[[15]], hang = -1)
-
-colnames(data) <- betti1$type
-
-dist <- diss(data[1:20], "DTWARP")
-
-h <- hclust(dist)
-par(cex=0.6)
-plot(h, hang = -1)
-
-
-cl <- kmeans(data[1:20,], 2)
-
-par(mar = c(9,9,9,9))
-plot(data[,1:20], col = cl$cluster)
-points(cl$centers, col = 1:2, pch = 8, cex = 2)
-
-
-#SOM
-gr <- somgrid(topo = "rectangular")
-res <- SOM(data, gr)
-
-plot(res)
+saveRDS(data, "data/mazda/processed/clust2.RDS")
+for(i in 1:15){
+  plot(data$clust[[i]], hang = -1)
+}
+for(i in 1:15){
+  ggdendrogram(data$clust[[i]], rotate=TRUE)+
+    theme_classic()+
+    labs(y = "DTW", x="")
+  ggsave()
+}
